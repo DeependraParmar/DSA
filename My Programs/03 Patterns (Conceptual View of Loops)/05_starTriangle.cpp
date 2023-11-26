362a603e71a7b41de92392bc092ff73bf077f286
DeependraParmar <deependraparmar1@gmail.com>
2023-11-26 17:35:09 +0530

    Star Triangle

--- My Programs/03 Patterns (Conceptual View of Loops)/05_starTriangle.cpp
+++ My Programs/03 Patterns (Conceptual View of Loops)/05_starTriangle.cpp
@@ -2,13 +2,19 @@
 using namespace std;
 
 int main(){
+    // * 
+    // * *
+    // * * *
+    // * * * *
+    // * * * * *
+
     int n;
     cout << "Enter the value of n: ";
     cin >> n;
-    int count = 1;
+
     for(int i=1; i<=n; i++){
-        for(int j=1; j<=n; j++){
-            cout << count++ << " ";
+        for(int j=1; j<=i; j++){
+            cout << "*" <<" ";
         }
         cout << endl;
     }
