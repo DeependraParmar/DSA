99ea4816bf30613634cb42d25023f4a01a73370d
DeependraParmar <deependraparmar1@gmail.com>
2023-11-26 08:57:02 +0530

    sizeOf() Operator

---
+++ My Programs/01 Basics of C++/03_sizeOf.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // We can use the sizeof() operator to find the size of a datatype or a variable. The sizeof() operator returns the size of the datatype or the variable in bytes. 
+
+    int a = 10;
+    float b = 10.5;
+    char c = 'A';
+
+    cout << "The size of int datatype is " << sizeof(a) << " bytes" << endl;
+    cout << "The size of float datatype is " << sizeof(b) << " bytes" << endl;
+    cout << "The size of char datatype is " << sizeof(c) << " bytes" << endl;
+    return 0;
+}
