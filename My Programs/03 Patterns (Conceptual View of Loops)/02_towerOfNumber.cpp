3d2b6c680077ac8512f2fbfc4a030fa5dc43a6e5
DeependraParmar <deependraparmar1@gmail.com>
2023-11-26 17:09:55 +0530

    Tower of Numbers

--- My Programs/03 Patterns (Conceptual View of Loops)/02_towerOfNumber.cpp
+++ My Programs/03 Patterns (Conceptual View of Loops)/02_towerOfNumber.cpp
@@ -2,19 +2,19 @@
 using namespace std;
 
 int main(){
-        // * * * * * 
-        // * * * * * 
-        // * * * * * 
-        // * * * * * 
-        // * * * * * 
-
+    // 1 1 1 
+    // 2 2 2
+    // 3 3 3
+    // 4 4 4
+    // 5 5 5
+    
     int n;
     cout << "Enter the value of n: ";
     cin >> n;
 
     for(int i=1; i<=n; i++){
-        for(int j=1; j<=n; j++){
-            cout << "*" << " ";
+        for(int j=1; j<=3; j++){
+            cout << i <<" ";
         }
         cout << endl;
     }
