fdeffaff331c0c75438f0d5173472247b9bc24c2
DeependraParmar <deependraparmar1@gmail.com>
2023-11-26 17:55:28 +0530

    Character Square

--- My Programs/03 Patterns (Conceptual View of Loops)/08_characterSquare.cpp
+++ My Programs/03 Patterns (Conceptual View of Loops)/08_characterSquare.cpp
@@ -2,21 +2,24 @@
 using namespace std;
 
 int main(){
-    // 1 
-    // 2 1
-    // 3 2 1
-    // 4 3 2 1
-    // 5 4 3 2 1
+    // A A A A A 
+    // B B B B B
+    // C C C C C
+    // D D D D D 
+    // E E E E E
 
     int n;
     cout << "Enter the value of n: ";
     cin >> n;
 
+    char ch = 'A';
+
     for(int i=1; i<=n; i++){
-        for(int j=1; j<=i; j++){
-            cout << i+j-1 << " ";
+        for(int j=1; j<=n; j++){
+            cout << ch <<" ";
         }
         cout << endl;
+        ch++;
     }
     return 0;
 }
