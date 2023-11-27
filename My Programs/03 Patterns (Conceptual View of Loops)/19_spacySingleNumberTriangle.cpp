762786e03b2c4498d7fb34f2e005099943d69e62
DeependraParmar <deependraparmar1@gmail.com>
2023-11-27 11:18:17 +0530

    Spacy Single Number Triangle

--- My Programs/03 Patterns (Conceptual View of Loops)/19_spacySingleNumberTriangle.cpp
+++ My Programs/03 Patterns (Conceptual View of Loops)/19_spacySingleNumberTriangle.cpp
@@ -2,6 +2,24 @@
 using namespace std;
 
 int main(){
-    
+    //      1
+    //     22
+    //    333
+    //   4444
+    //  55555
+
+    int n;
+    cout << "Enter the value of n: ";
+    cin >> n;
+
+    for(int i=n; i>=1; i--){
+        for(int j=i; j>=1; j--){
+            cout << " ";
+        }
+        for(int j=n-i+1; j>=1; j--){
+            cout << n-i+1;
+        }
+        cout << endl;
+    }   
     return 0;
 }
