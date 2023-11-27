08fe1f276c679f5ac5f92748e34b7f217ecbf33a
DeependraParmar <deependraparmar1@gmail.com>
2023-11-27 10:55:42 +0530

    Spacy Inverted Star Triangle

--- My Programs/03 Patterns (Conceptual View of Loops)/15_spacyInvertedStarTriangle.cpp
+++ My Programs/03 Patterns (Conceptual View of Loops)/15_spacyInvertedStarTriangle.cpp
@@ -2,20 +2,22 @@
 using namespace std;
 
 int main(){
-    // D 
-    // C D
-    // B C D
-    // A B C D
+    //     *
+    //    **
+    //   ***
+    //  ****
+    // *****
 
     int n;
     cout << "Enter the value of n: ";
     cin >> n;
 
-    char ch = 'A';
-
     for(int i=1; i<=n; i++){
-        for(int j=1; j<=i; j++){
-            cout << char(ch+n-i+j-1) << " ";
+        for(int j=1; j<=n-i; j++){
+            cout << " ";
+        }
+        for(int k=1; k<=i; k++){
+            cout << "*";
         }
         cout << endl;
     }
