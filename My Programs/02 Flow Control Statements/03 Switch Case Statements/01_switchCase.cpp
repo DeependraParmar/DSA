17605c457620a947bd97bb57ce33bc909104b196
DeependraParmar <deependraparmar1@gmail.com>
2023-11-28 13:19:28 +0530

    Switch Case Statements

---
+++ My Programs/02 Flow Control Statements/03 Switch Case Statements/01_switchCase.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // Writing a switch case statement for months and their corresponding number 
+    int n;
+    cout << "Enter the number of the month: ";
+    cin >> n;
+    
+    switch(n){
+        case 1:
+            cout << "January" << endl;
+            break;
+        case 2:
+            cout << "February" << endl;
+            break;
+        case 3:
+            cout << "March" << endl;
+            break;
+        case 4:
+            cout << "April" << endl;
+            break;
+        case 5:
+            cout << "May" << endl;
+            break;
+        case 6:
+            cout << "June" << endl;
+            break;
+        case 7:
+            cout << "July" << endl;
+            break;
+        case 8:
+            cout << "August" << endl;
+            break;
+        case 9:
+            cout << "September" << endl;
+            break;
+        case 10:
+            cout << "October" << endl;
+            break;
+        case 11:
+            cout << "November" << endl;
+            break;
+        case 12:
+            cout << "December" << endl;
+            break;
+        default:
+            cout << "Invalid Input" << endl;
+    }
+    return 0;
+}
