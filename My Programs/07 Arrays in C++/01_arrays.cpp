a59c023d747c72f67daca85fe2ed9ee444d91d9f
DeependraParmar <deependraparmar1@gmail.com>
2023-11-29 16:38:16 +0530

    Arrays

---
+++ My Programs/07 Arrays in C++/01_arrays.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int arr[5]; // declaring an array
+
+    // initialising an array 
+    arr[0] = 10;
+    arr[1] = 20;
+    arr[2] = 30;
+    arr[3] = 40;
+    arr[4] = 50;
+
+    // accessing an array
+    cout << arr[0] << endl;
+    cout << arr[1] << endl;
+    cout << arr[2] << endl;
+    cout << arr[3] << endl;
+    cout << arr[4] << endl;   
+    return 0;
+}
