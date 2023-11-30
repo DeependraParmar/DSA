f8587af09a5e2c2e11cece805d73a7becc057415
DeependraParmar <deependraparmar1@gmail.com>
2023-11-30 14:08:39 +0530

    Sort 0,1

---
+++ CodeStudios/Sort 0,1.cpp
@@ -0,0 +1,18 @@
+/*
+    void sortZeroesAndOne(int input[], int size)
+{
+    //Write your code here
+    int i=0,j=0;
+
+    while(i<size){
+        if(input[i] == 0){
+            swap(input[i],input[j]);
+            i++;
+            j++;
+        }
+        else{
+            i++;
+        }
+    }
+}
+*/
