cc15f953173f0a3a360c9448748a33149cb6ecce
DeependraParmar <deependraparmar1@gmail.com>
2023-12-10 17:34:32 +0530

    Bubble Sort

--- My Programs/09 Sorting Algorithms/02_bubbleSort.cpp
+++ My Programs/09 Sorting Algorithms/02_bubbleSort.cpp
@@ -5,9 +5,9 @@ int main(){
     int arr[6] = {10,8,7,3,9,1};
     int n = 6;
 
-    for(int i=1; i<n; i++){
+    for(int i=0; i<n-1; i++){
         int flag = 0;
-        for(int j=0; j<n-1; j++){
+        for(int j=0; j<n-i-1; j++){
             if(arr[j] > arr[j+1]){
                 swap(arr[j], arr[j+1]);
                 flag = 1;
