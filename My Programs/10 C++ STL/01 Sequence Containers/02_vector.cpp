24ca34add33e2ba74330731f89207558b27fc74e
DeependraParmar <deependraparmar1@gmail.com>
2023-12-13 03:46:02 +0530

    Vectors in STL

---
+++ My Programs/10 C++ STL/01 Sequence Containers/02_vector.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    vector<int> arr; // normal vector
+    vector<int> sizedVector(10,1); // vector of size 10, initialised with 1.
+
+    cout << "Capacity of vector arr: " << arr.capacity() << endl;
+
+    // Pushing an element in the array 
+    arr.push_back(1);
+    cout << "Capacity: " << arr.capacity() << endl;
+
+    arr.push_back(2);
+    cout << "Capacity: " << arr.capacity() << endl;
+
+    arr.push_back(3);
+    cout << "Capacity: " << arr.capacity() << endl;
+
+
+    // Element at direct index 
+    cout << "Element at 2nd index is: " << arr.at(2) << endl;
+
+
+    // Finding front 
+    cout << "Front element: " << arr.front() << endl;
+
+    // Finding back 
+    cout << "Back element: " << arr.back() << endl;
+
+
+    // Popping element from vector 
+    cout << endl << "Vector before Pop: ";
+    for(int i:arr){
+        cout << i << " ";
+    }
+
+    arr.pop_back();
+
+    cout << endl << "Vector after Pop: ";
+    for(int i:arr){
+        cout << i << " ";
+    }
+
+
+    cout << endl << "Size before clear: " << arr.size() << endl;
+    arr.clear();
+    cout << endl << "Size after clear: " << arr.size() << endl;
+
+
+    return 0;
+}
