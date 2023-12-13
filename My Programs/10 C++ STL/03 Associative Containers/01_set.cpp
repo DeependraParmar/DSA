7f012b96e6ef318ce236c6413ef9ae3f3329e0f5
DeependraParmar <deependraparmar1@gmail.com>
2023-12-13 05:31:27 +0530

    Sets in STL

--- My Programs/10 C++ STL/03 Associative Containers/01_set.cpp
+++ My Programs/10 C++ STL/03 Associative Containers/01_set.cpp
@@ -13,10 +13,25 @@ int main(){
     s.insert(3);
     s.insert(0);
 
-    cout << "Set looks like: "
+    cout << "Set looks like: ";
     for(auto i:s){
         cout << i <<" ";
     }
 
+    set<int>::iterator it = s.begin();
+    it++;
+
+    s.erase(it);
+    cout << endl << endl << "Set after erase operation: ";
+    for(auto i:s){
+        cout << i << " ";
+    }
+
+    cout << endl << endl << "Is 5 there? -> " << s.count(5) << endl;
+    cout << "Is 7 there? -> " << s.count(7) << endl;
+
+
+    set<int>::iterator findItr = s.find(5);
+    cout << "Value stored at iterator is: " << *findItr << endl;
     return 0;
 }
