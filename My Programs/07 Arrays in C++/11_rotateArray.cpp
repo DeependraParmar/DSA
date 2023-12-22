af8bda4ed25f1e216ae0cd195741bd5ee6608e74
DeependraParmar <deependraparmar1@gmail.com>
2023-12-22 09:59:01 +0530

    Rotate Array

---
+++ My Programs/07 Arrays in C++/11_rotateArray.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int arr[8] = {3,1,5,8,4,9,7,2};
+    int n = 8;
+    int k;
+    cout << "Enter the degree of rotation: ";
+    cin >> k;
+
+    
+
+    return 0;
+}
