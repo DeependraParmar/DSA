995e3276911e727e69b2810dcc64535c6abbdb48
DeependraParmar <deependraparmar1@gmail.com>
2023-12-22 12:04:04 +0530

    Check if a String is Palindrome

---
+++ LeetCode/Check if a String is Palindrome.cpp
@@ -0,0 +1,72 @@
+/*
+#include <bits/stdc++.h>
+#include <cstring>
+
+bool isValid(char ch){
+
+    if(ch>='a'&&ch<='z' || ch>='A'&&ch<='Z' || ch>='0'&&ch<='9'){
+
+        return true;
+
+    }
+
+    return false;
+
+}
+
+
+
+bool checkPalindrome(string s)
+
+{
+
+    int start = 0;
+
+    int end = s.length()-1;
+
+
+
+    while(start<=end){
+
+        if(isValid(s[start])){
+
+            if(isValid(s[end])){
+
+                if( tolower(s[start]) != tolower(s[end]) ){
+
+                    return false;
+
+                }
+
+                else{
+
+                    start++;
+
+                    end--;
+
+                }
+
+            }
+
+            else{
+
+                end--;
+
+            }
+
+        }
+
+        else{
+
+            start++;
+
+        }
+
+    }
+
+
+
+    return true;
+
+}
+*/
