388581baf15bb257d79573682c89ef9e284f16b2
DeependraParmar <deependraparmar1@gmail.com>
2023-12-26 20:37:43 +0530

    Pointers

--- My Programs/14 Pointers in C++/01_pointers.cpp
+++ My Programs/14 Pointers in C++/01_pointers.cpp
@@ -6,5 +6,12 @@ int main(){
     cout << "Address of num is: " << &num << endl;   
     cout << "Value stored at num is: " << *(&num) << endl; 
 
+    int *ptr = &num;
+    cout << "Value stored by pointer is: " << ptr << endl;
+    cout << "Value stored at address stored by pointer is: " << *ptr << endl;
+
+    cout << endl << "Size of num is: " << sizeof(num) << endl;
+    cout << "Size of pointer is: " << sizeof(ptr) << endl;
+
     return 0;
 }
