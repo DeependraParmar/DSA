b01e0c508db2cf384982de16665d95a4ac44383f
DeependraParmar <deependraparmar1@gmail.com>
2023-12-26 16:32:05 +0530

    LCM

---
+++ My Programs/13 Math for DSA/03_lcm.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+int gcd(int a, int b){
+    if(a==0){
+        return b;
+    }
+    if(b==0){
+        return a;
+    }
+
+    while(a!=b){
+        if(a>b){
+            a-=b;
+        }
+        else{
+            b-=a;
+        }
+    }
+
+    return a;
+}
+int lcm(int a, int b){
+    return (a*b)/(gcd(a,b));
+}
+int main(){
+    int a,b;
+    cout << "Enter the value of a: ";
+    cin >> a;
+    cout << "Enter the value of b: ";
+    cin >> b;
+
+    cout << "LCM of " << a << " and " << b << " is: " << lcm(a,b) << endl;
+   
+    return 0;
+}
