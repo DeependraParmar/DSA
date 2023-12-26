82d9e69dfd7b9a5137b0e79a37f88af47f601eb2
DeependraParmar <deependraparmar1@gmail.com>
2023-12-26 21:01:25 +0530

    Pointer Arithmetic

---
+++ My Programs/14 Pointers in C++/04_pointerArithmetic.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num = 5;
+    int *ptr = &num;
+
+    cout << "ptr before: " << ptr << endl;
+    ptr = ptr + 1;
+    cout << "ptr after: " << ptr << endl;
+
+    return 0;
+}
