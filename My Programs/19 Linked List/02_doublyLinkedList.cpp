b54af384386248c7cc0b785aae49f84e298578ee
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-01-19 05:53:06 +0530

    Deletion from first, last and any position from a linked list

--- My Programs/19 Linked List/02_doublyLinkedList.cpp
+++ My Programs/19 Linked List/02_doublyLinkedList.cpp
@@ -11,6 +11,14 @@ class Node{
         this->data = data;
         this->next = NULL;
     }
+    ~Node(){
+        int value = this->data;
+        if(this->next != NULL){
+            delete next;
+            next = NULL;
+        }
+        cout << "memory cleared for " << value << endl;
+    }
 };
 
 Node* head = NULL;
@@ -72,7 +80,43 @@ void print(){
     }
     cout << endl;
 }
+void deleteNode(int pos){
+    if(pos<=0 || pos>n){
+        cout << "Invalid Position input for Deletion" << endl;
+    }
+    else if(pos == 1){
+        temp = head;
+        temp->next->prev = NULL;
+        head = temp->next;
+        temp->next = NULL;
+        n--;
+        delete temp;
+        return;
+    }
+    else if(pos == n){
+        temp = tail;
+        temp->prev->next = NULL;
+        tail = temp->prev;
+        temp->prev = NULL;
+        n--;
+        delete temp;
+    }
+    else{
+        temp = head;
+        int i=1;
 
+        while(i<pos){
+            temp = temp->next;
+            i++;
+        }
+        temp->next->prev = temp->prev;
+        temp->prev->next = temp->next;
+        temp->next = NULL;
+        temp->prev = NULL;
+        n--;
+        delete temp;
+    }
+}
 
 int main(){
     Node* node1 = new Node(9);
@@ -89,7 +133,14 @@ int main(){
     insertAtPosition(99,6); // inserting node at last position
     // insertAtPosition(19,99); // invalid position for insertion case
 
+    cout << endl;
+    print();
+    cout << "Total Nodes in LL are: " << n << endl;
+
     cout << endl << endl;
+    deleteNode(1); // deleting the first node
+    deleteNode(5); // deleting the last node
+    deleteNode(3); // deleting the node at position 3
     print();
     cout << "Total Nodes in LL are: " << n << endl;
     return 0;
