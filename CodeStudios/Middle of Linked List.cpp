d0d80de1b9db034862052ac70357b1d54f19415a
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-01-30 05:43:52 +0530

    Middle of a linked list: Pro approach

--- CodeStudios/Middle of Linked List.cpp
+++ CodeStudios/Middle of Linked List.cpp
@@ -36,3 +36,28 @@ Node *findMiddle(Node *head) {
     return getMid(head);
 }
 */
+
+
+// More Pro approach
+/*
+Node *findMiddle(Node *head) {
+
+    if(head == NULL || head->next == NULL){
+        return head;
+    }
+
+
+    Node* slow = head;
+    Node* fast = head->next;
+
+    while(fast != NULL){
+        fast = fast->next;
+        if(fast != NULL){
+            fast = fast->next;
+        }
+        slow = slow->next;
+    }
+
+    return slow;
+}
+*/
