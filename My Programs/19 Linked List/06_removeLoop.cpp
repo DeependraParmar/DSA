babe21dfadb7d0c21c113dc5ac2f7a286e4308f0
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-04-10 12:29:51 +0530

    Remove loop/cycle from linked list

---
+++ My Programs/19 Linked List/06_removeLoop.cpp
@@ -0,0 +1,12 @@
+/*
+void removeLoop(Node* head){
+    Node* start = getStartingNode(head);
+    Node* temp = start;
+
+    while(temp -> next != start){
+        temp ->= next;
+    }
+
+    temp -> next = NULL;
+}
+*/
