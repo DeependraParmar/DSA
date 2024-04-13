1ebec1b465f1a53634b80b1964593d883be2afd0
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-04-13 11:41:34 +0530

    Add two numbers represented by linked list single loop: optimal approach

---
+++ GeeksForGeeks/Add two numbers represented by linked list optimal.cpp
@@ -0,0 +1,84 @@
+/*
+class Solution{
+    private:
+    Node* reverseLinkedList(Node* head){
+        Node* current = head;
+        Node* prev = NULL;
+        Node* next = NULL;
+
+        while(current != NULL){
+            next = current -> next;
+            current -> next = prev;
+            prev = current;
+            current = next;
+        }
+
+        return prev;
+    }
+
+    void insertAtTail(Node* &head, Node* &tail, int val){
+        Node* temp = new Node(val);
+
+        if(head == NULL){
+            head = temp;
+            tail = temp;
+            return;
+        }
+        else{
+            tail -> next = temp;
+            tail = temp;
+        }
+    }
+
+    Node* add(Node* first, Node* second){
+        int carry = 0;
+        int sum = 0;
+        int digit = 0;
+        Node* ansHead = NULL;
+        Node* ansTail = NULL;
+
+        while(first != NULL || second != NULL || carry != 0){
+            int value1 = 0, value2 = 0;
+            if(first != NULL)
+                value1 = first -> data;
+
+            if(second != NULL)
+                value2 = second -> data;
+
+            sum = carry + value1 + value2;
+            digit = sum % 10;
+            insertAtTail(ansHead, ansTail, digit);
+            carry = sum/10;
+
+            if(first != NULL)
+                first = first -> next;
+
+            if(second != NULL)
+                second = second -> next;
+        }
+
+        return ansHead;
+    }
+    public:
+    //Function to add two numbers represented by linked list.
+    struct Node* addTwoLists(struct Node* num1, struct Node* num2){
+
+        // step 01: reversing the linked list
+        Node* first = reverseLinkedList(num1);
+        Node* second = reverseLinkedList(num2);
+
+        // step 02: start adding them
+        Node* ans = add(first, second);
+
+        // step 03: reversing the ans linked list
+
+        ans = reverseLinkedList(ans);
+
+        while(ans -> data == 0 && ans->next != NULL ){
+            ans = ans -> next;
+        }
+
+        return ans;
+    }
+};
+*/
