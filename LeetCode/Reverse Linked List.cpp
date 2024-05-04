a3592d6c895e05e2087a4a5e27b89c5189aa64a0
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-05-04 12:28:27 +0530

    Reverse linked list

---
+++ LeetCode/Reverse Linked List.cpp
@@ -0,0 +1,19 @@
+/*
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode *prev = NULL;
+        ListNode *current = head;
+        ListNode *next = NULL;
+
+        while(current != NULL){
+            next = current -> next;
+            current -> next = prev;
+            prev = current;
+            current = next;
+        }
+
+        return prev;
+    }
+};
+*/
