5c211e6c1e98f1c5236102b4730ff5c080323a4c
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-06-04 13:12:39 +0530

    Deletion from BST

--- My Programs/23 Binary Search Trees/01_implementation.cpp
+++ My Programs/23 Binary Search Trees/01_implementation.cpp
@@ -110,6 +110,49 @@ Node* max(Node* root){
 
     return temp;
 }
+
+Node* deleteFromBST(Node* root, int val){
+    if(root == NULL)
+        return NULL;
+    
+    if(root -> data == val){
+        // lead node
+        if(root -> left == NULL && root -> right == NULL){
+            delete root;
+            return NULL;
+        }
+
+        // left child
+        if(root -> left && !root -> right){
+            Node* temp = root -> left;
+            delete root;
+            return temp;
+        }
+
+        // right child
+        if(!root -> left && root -> right){
+            Node* temp = root -> right;
+            delete root;
+            return temp;
+        }
+
+        // root with two child
+        if(root -> left && root -> right){
+            int mini = min(root ->right)-> data;
+            root -> data = mini;
+            root -> right = deleteFromBST(root -> right, mini);
+            return root;
+        }
+    }
+    else if(root -> data > val){
+        root -> left = deleteFromBST(root -> left, val);
+        return root;
+    }
+    else{
+        root -> right = deleteFromBST(root -> right, val);
+        return root;
+    }
+}
 int main(){
     Node* root = NULL;
     takeInput(&root); // Pass the address of the root node
