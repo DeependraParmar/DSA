4a121a952ddfdf5d6b3b6ba22d103d2927767766
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-06-05 10:07:25 +0530

    Floor in a sorted array: finding lower bound

--- GeeksForGeeks/Floor in a Sorted Array.cpp
+++ GeeksForGeeks/Floor in a Sorted Array.cpp
@@ -1,23 +1,33 @@
-/*
-class Solution {
-public:
-    int search(vector<int>& nums, int target) {
-        int start = 0;
-        int end = nums.size() - 1;
-        int mid = start + (end-start)/2;
+// class Solution
+// {
+// public:
+//     // Function to find floor of x
+//     // n: size of vector
+//     // x: element whose floor is to find
+//     int findFloor(vector<long long> arr, long long n, long long x)
+//     {
+//         int start = 0;
+//         int end = n - 1;
+//         int mid = start + (end - start) / 2;
+//         int ans = -1;
 
-        while(start <= end){
-            if(nums[mid] == target)
-                return mid;
-            else if(nums[mid] > target)
-                end = mid - 1;
-            else
-                start = mid + 1;
+//         while (start <= end)
+//         {
+//             if (arr[mid] == x)
+//                 return mid;
 
-            mid = start + (end-start)/2;
-        }
+//             else if (arr[mid] > x)
+//                 end = mid - 1;
 
-        return -1;
-    }
-};
-*/
+//             else
+//             {
+//                 start = mid + 1;
+//                 ans = mid;
+//             }
+
+//             mid = start + (end - start) / 2;
+//         }
+
+//         return ans;
+//     }
+// };
