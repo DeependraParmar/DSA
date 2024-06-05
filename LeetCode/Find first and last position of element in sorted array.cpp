e9d69f1d16eb5f073fd57c3f6dd89430269420d6
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-06-05 11:29:06 +0530

    Find the first and last occurence of an element in sorted array

---
+++ LeetCode/Find first and last position of element in sorted array.cpp
@@ -0,0 +1,55 @@
+/*
+class Solution {
+public:
+    int firstOccurence(vector<int> &nums, int target){
+        int start = 0;
+        int end = nums.size() - 1;
+        int mid = start + (end-start)/2;
+        int ans = -1;
+
+        while(start <= end){
+            if(nums[mid] == target){
+                ans = mid;
+                end = mid-1;
+            }
+            else if(nums[mid] > target)
+                end = mid-1;
+            else
+                start = mid+1;
+
+            mid = start + (end-start)/2;
+        }
+        return ans;
+    }
+    int lastOccurence(vector<int> &nums, int target){
+        int start = 0;
+        int end = nums.size() - 1;
+        int mid = start + (end-start)/2;
+        int ans = -1;
+
+        while(start <= end){
+            if(nums[mid] == target){
+                ans = mid;
+                start = mid+1;
+            }
+            else if(nums[mid] > target)
+                end = mid-1;
+            else
+                start = mid+1;
+
+            mid = start + (end-start)/2;
+        }
+        return ans;
+    }
+    vector<int> searchRange(vector<int>& nums, int target) {
+        vector<int> ans;
+
+        int first = firstOccurence(nums, target);
+        int last = lastOccurence(nums, target);
+
+        ans.push_back(first);
+        ans.push_back(last);
+        return ans;
+    }
+};
+*/
