6af2ae7c199874c229e7cede49a016eb41df5b5b
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-06-06 07:06:46 +0530

    Single element in sorted array

---
+++ LeetCode/Single Element in Sorted Array.cpp
@@ -0,0 +1,14 @@
+/*
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        int ans = 0;
+        for(int i=0; i<nums.size(); i++){
+            ans = ans ^ nums[i];
+        }
+        return ans;
+    }
+};
+*/
