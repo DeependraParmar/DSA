a499e234ad5dc28ab9aaefffa21e7953fb677b88
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-06-07 12:10:31 +0530

    Heap Implementation

--- My Programs/24 Heaps/01_implementation.cpp
+++ My Programs/24 Heaps/01_implementation.cpp
@@ -4,7 +4,12 @@ using namespace std;
 class Heap{
     public:
         int arr[100];
-        int size = 0;
+        int size;
+
+        Heap(){
+            arr[0] = -1;
+            size = 0;
+        }
 
         void insert(int val){
             size += 1;
