d01ce80b271551c4e7aecbaef889d47c0a325d6f
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-06-13 08:28:17 +0530

    Traversal in Linked List

---
+++ GeeksForGeeks/Length of Linked List.cpp
@@ -0,0 +1,20 @@
+/*
+class Solution
+{
+    public:
+    //Function to count nodes of a linked list.
+    int getCount(struct Node* head){
+
+        Node* temp = head;
+        int n = 0;
+
+        while(temp != NULL){
+            temp = temp -> next;
+            n++;
+        }
+
+        return n;
+
+    }
+};
+*/
