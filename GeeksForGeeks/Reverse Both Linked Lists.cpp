983b8257c89912f16edfa19f5817215418fdd93e
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-06-25 16:16:28 +0530

    Reverse both parts of linked list at point k

---
+++ GeeksForGeeks/Reverse Both Linked Lists.cpp
@@ -0,0 +1,49 @@
+/*
+class Solution
+{
+public:
+    Node* reversedLL(Node* head){
+        if(head -> next == NULL)
+            return head;
+
+        Node* prev = NULL;
+        Node* temp = head;
+        Node* next = NULL;
+
+        while(temp){
+            next = temp -> next;
+            temp -> next = prev;
+            prev = temp;
+            temp = next;
+        }
+
+        return prev;
+    }
+    Node *reverse(Node *head, int k)
+    {
+        if(head -> next == NULL)
+            return head;
+
+        Node* temp = head;
+        Node* head1 = head;
+        Node* head2 = NULL;
+
+        // dividing one into two distinct linked lists
+        for(int i=1; i<k; i++){
+            temp = temp -> next;
+        }
+
+        // saving the head of 2nd part of linked list
+        head2 = temp -> next;
+        temp -> next = NULL;
+
+        // reversing both the linked lists seperately
+        Node* ansHead = reversedLL(head1);
+        Node* secondHead = reversedLL(head2);
+
+        // connecting both the reversed linked lists
+        head -> next = secondHead;
+        return ansHead;
+    }
+};
+*/
