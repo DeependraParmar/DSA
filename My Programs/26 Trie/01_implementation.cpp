98f44750ffc7d13108386bdeacfe5dbb36f1b3e7
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-07-01 05:16:59 +0530

    Trie & Its implementation: Insertion in Trie

---
+++ My Programs/26 Trie/01_implementation.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+class TrieNode{
+    public:
+        char data;
+        TrieNode* children[26];
+        bool isTerminal;
+    
+    TrieNode(char ch){
+        this->data = ch;
+        this->isTerminal = false;
+
+        for(int i=0; i<26; i++){
+            children[i] = NULL;
+        }
+    }
+};
+
+class Trie{
+    public: 
+        TrieNode* root;
+        
+    Trie(){
+        root = new TrieNode('\0');
+    }
+
+    void insertUtil(TrieNode* root, string word){
+        if(word.length() == 0){
+            root -> isTerminal = true;
+            return;
+        }
+
+        int index = word[0] - 'A';
+        TrieNode* child;
+
+        // present
+        if(root -> children[index] != NULL){
+            child = root -> children[index];
+        }
+        else{
+            child = new TrieNode(word[0]);
+            root -> children[index] = child;
+        }
+
+        insertUtil(child, word.substr(1));
+    }
+    void insertWord(string word){
+        insertUtil(root, word);
+    }
+};
+int main(){
+    Trie *t  = new Trie();
+    t->insertWord("HELLO");
+    return 0;
+}
