2dcbba9bd9126c484badd4a0f68bb0030382b0cf
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-09-23 11:59:24 +0530

    Power of a number

---
+++ GeeksForGeeks/Power of a number.cpp
@@ -0,0 +1,16 @@
+/*
+if(n < 0)
+            return false;
+
+        int count = 0;
+
+        while(n != 0){
+            int bit = n&1;
+            if(bit == 1)
+                count += 1;
+
+            n >>= 1;
+        }
+
+        return count == 1 ? true : false;
+*/
