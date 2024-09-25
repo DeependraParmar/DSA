491a84f3eea2ec86af51b9d03650e64f99bad8ca
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-09-25 15:09:02 +0530

    Subsets: Leetcode

--- LeetCode/Subsets.cpp
+++ LeetCode/Subsets.cpp
@@ -1,28 +1,25 @@
 /*
 class Solution {
-private:
-void solve(vector<int> nums, vector<int> output, int index, vector<vector<int>>& ans){
-    // base case
-    if(index >= nums.size()){
-        ans.push_back(output);
-        return;
-    }
-
-    // exclude
-    solve(nums, output, index+1,ans);
-
-    // include
-    output.push_back(nums[index]);
-    solve(nums,output,index+1,ans);
-}
 public:
+    void solve(int i, vector<int>& arr, vector<int>& ds, vector<vector<int>>& ans){
+        if(i == arr.size()){
+            ans.push_back(ds);
+            return;
+        }
+
+        // TAKE
+        ds.push_back(arr[i]);
+        solve(i+1, arr, ds, ans);
+        ds.pop_back();
+
+        // NOT TAKE
+        solve(i+1, arr, ds, ans);
+    }
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> ans;
-        vector<int> output;
+        vector<int> ds;
 
-        int index = 0;
-        solve(nums, output,index, ans);
-
+        solve(0, nums, ds, ans);
         return ans;
     }
 };
