bb0e7346eaae613cc843af097e67a332aa9071bd
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-09-25 20:35:41 +0530

    Permutations Optimal approach: Leetcode

---
+++ LeetCode/Permutations Optimal.cpp
@@ -0,0 +1,24 @@
+/*
+class Solution {
+public:
+    void solve(int index, vector<int>& arr,vector<vector<int>>& ans){
+        if(index == arr.size()){
+            ans.push_back(arr);
+            return;
+        }
+
+        for(int i=index; i<arr.size(); i++){
+            swap(arr[index], arr[i]);
+            solve(index+1, arr, ans);
+            swap(arr[index], arr[i]);
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> ds;
+        solve(0, nums, ans);
+
+        return ans;
+    }
+};
+*/
