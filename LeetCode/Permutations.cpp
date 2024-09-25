6d5a1f79c57d86d810d5e29affa3cddb7c4340fe
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-09-25 20:18:03 +0530

    Permutations: Leetcode

--- LeetCode/Permutations.cpp
+++ LeetCode/Permutations.cpp
@@ -1,23 +1,29 @@
 /*
 class Solution {
-private:
-void solve(vector<int> nums, int index, vector<vector<int>>& ans){
-    if(index >= nums.size()){
-        ans.push_back(nums);
-        return;
-    }
-
-    for(int i=index; i<nums.size(); i++){
-        swap(nums[index],nums[i]);
-        solve(nums,index+1,ans);
-        swap(nums[index],nums[i]);
-    }
-}
 public:
+    void solve(vector<int>& arr, vector<int>& ds, vector<vector<int>>& ans, unordered_map<int, bool>& mapping){
+        if(ds.size() == arr.size()){
+            ans.push_back(ds);
+            return;
+        }
+
+        for(int i=0; i<arr.size(); i++){
+            if(!mapping[i]){
+                mapping[i] = true;
+                ds.push_back(arr[i]);
+                solve(arr, ds, ans, mapping);
+                ds.pop_back();
+                mapping[i] = false;
+            }
+        }
+    }
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
-        int index = 0;
-        solve(nums, index,ans);
+        vector<int> ds;
+        unordered_map<int,bool> mapping;
+
+        solve(nums, ds, ans, mapping);
+
         return ans;
     }
 };
