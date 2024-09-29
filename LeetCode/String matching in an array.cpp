f6c9106e028d73b7e3a7d37a56e8d2dbbc25a89b
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-09-29 14:40:29 +0530

    String matching in an array

---
+++ LeetCode/String matching in an array.cpp
@@ -0,0 +1,22 @@
+/*
+class Solution {
+public:
+    vector<string> stringMatching(vector<string>& words) {
+        vector<string> ans;
+
+        for(int i=0; i<words.size(); i++){
+            for(int j=0; j<words.size(); j++){
+                if(i==j || words[i].length() > words[j].length())
+                    continue;
+
+                if(words[j].find(words[i]) != string::npos){
+                    if(find(ans.begin(), ans.end(), words[i]) == ans.end())
+                        ans.push_back(words[i]);
+                }
+            }
+        }
+
+        return ans;
+    }
+};
+*/
