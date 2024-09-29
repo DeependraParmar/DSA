44e944d1878161d10e67327902df4dd2f4b0cbc7
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-09-29 14:42:41 +0530

    Find the first palindromic string in the array

---
+++ LeetCode/Find first palindromic string in the array.cpp
@@ -0,0 +1,24 @@
+/*
+class Solution {
+public:
+    bool isPalindrome(string s){
+        int start = 0;
+        int end = s.length() - 1;
+
+        while(start <= end){
+            if(s[start++] != s[end--])
+                return false;
+        }
+
+        return true;
+    }
+    string firstPalindrome(vector<string>& words) {
+        for(int i=0; i<words.size(); i++){
+            if(isPalindrome(words[i]))
+                return words[i];
+        }
+
+        return "";
+    }
+};
+*/
